58ef1ca95821254556a2a5ecf7473e7a4e381c5b
LarsSong1 <damianalejandrofirst@gmail.com>
2024-10-21 16:50:39 -0500

    doing-frontend

--- app/(routes)/category/[categoryId]/components/mobile-flters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-flters.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import Button from "@/components/ui/Button";
+import IconButton from "@/components/ui/icon-button";
 import { Color, Size } from "@/types"
 import { Dialog } from "@headlessui/react";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { useState } from "react";
+import Filter from "./filter";
 
 
 interface MobileFiltersProps {
@@ -31,13 +33,29 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
             </Button>
             <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
                 {/* Background */}
-                <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
                 {/* Dialog position */}
                 <div className="fixed inset-0  z-40 flex">
                     <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl ">
                         {/* Close  */}
                         <div className="flex items-center justify-end px-4">
+                            <IconButton icon={<X size={15} />}
+                                onClick={onClose}
+                            />
+                        </div>
 
+                        {/* Render the filters */}
+                        <div className="p-4">
+                            <Filter
+                                valueKey="sizeId"
+                                name="Tamaños"
+                                data={sizes}
+                            />
+                            <Filter
+                                valueKey="colorId"
+                                name="Colores"
+                                data={colors}
+                            />
                         </div>
                     </Dialog.Panel>
 
